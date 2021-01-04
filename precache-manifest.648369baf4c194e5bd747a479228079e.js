fcd6c17de7a7adc7ca92036a847736a312b5a723
Aston13 <Astonoturner@gmail.com>
2021-01-04 23:55:27 +0100

    deploy

---
+++ precache-manifest.648369baf4c194e5bd747a479228079e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46d2f710d33ed7800423",
+    "url": "/github-pages/css/app.98a263f3.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/github-pages/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "676504837645137eb24e7ff934dab230",
+    "url": "/github-pages/index.html"
+  },
+  {
+    "revision": "3436ea3ac82482f3e3a0",
+    "url": "/github-pages/js/about.063687bd.js"
+  },
+  {
+    "revision": "46d2f710d33ed7800423",
+    "url": "/github-pages/js/app.e861a638.js"
+  },
+  {
+    "revision": "03e302cb671156ef5247",
+    "url": "/github-pages/js/chunk-vendors.1721e4fb.js"
+  },
+  {
+    "revision": "fea14d6dede60254987832968bf6087d",
+    "url": "/github-pages/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/github-pages/robots.txt"
+  }
+]);
